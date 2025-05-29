baeb98c3f2c3a7a9d5ecf25c4eb85ab902420157
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-05-29 22:12:45 +0600

    Ready for Deployment

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,13 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/contact", contactRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
